01a2fb750fc9da3434ed0313c14f498c30f1519c
Troupote <thybault.jallu@icloud.com>
2025-03-02 02:53:39 +0100

    [Feat] Modified startup and sound players to work with Fmod

--- Assets/Script/SoundPlayerManager.cs
+++ Assets/Script/SoundPlayerManager.cs
@@ -1,4 +1,6 @@
 using DG.Tweening;
+using FMOD.Studio;
+using FMODUnity;
 using NUnit.Framework;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -10,14 +12,32 @@ public class SoundPlayerManager : MonoBehaviour
     private Collider LeCollider;
 
     [SerializeField]
-    private MusicSetupSO MusicSO;
-
-    [SerializeField]
     private int PartOfSong;
-  
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Le collider " + LeCollider.name + " est trigger.");
-        MusicSO.audioSources[PartOfSong - 1].DOFade(1f, 0.8f);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Le collider " + LeCollider.name + " est trigger.");
+
+            // Jouez l'événement FMOD correspondant à la partie de la chanson
+            if (PartOfSong > 0)
+            {
+                var musicEventInstance = FMODInstanceManager.instance.GetMusicEventInstance(PartOfSong - 1);
+                if (musicEventInstance.isValid())
+                {
+                    // Définir les attributs 3D pour l'instance de musique
+                    var attributes = RuntimeUtils.To3DAttributes(Vector3.zero);
+                    musicEventInstance.set3DAttributes(attributes);
+
+                    musicEventInstance.setVolume(1f);
+                    Debug.Log("Playing part of song: " + PartOfSong);
+                }
+            }
+            else
+            {
+                Debug.LogError("PartOfSong index is out of range.");
+            }
+        }
     }
 }
