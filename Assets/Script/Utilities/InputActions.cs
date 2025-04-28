f0309f63e284305414cf0031c6fd4cab59cfe841
Furydrack <marius.boulandet@gmail.com>
2025-04-28 12:25:40 +0200

    [FEAT] Player Input Management
    - "It's been 3.000 years..." ahh commit
    - All necessaries for Player input management, including Players Input Manager and his prefab, Player Input Controller/Receiver scripts and their prefab, ControllerSelection UI, Input Actions class and SO (for retrieving Actions without a string, it's my version of the C# generated class), and enums

---
+++ Assets/Script/Utilities/InputActions.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace BHR
+{
+    public static class InputActions
+    {
+        public static string KeyboardScheme;
+        public static string GamepadScheme;
+
+        public static string HumanoidActionMap;
+        public static string SingularityActionMap;
+        public static string UIActionMap;
+        public static string InactiveActionMap;
+
+        public static string Move;
+        public static string Look;
+        public static string Jump;
+        public static string Dash;
+        public static string Slide;
+        public static string Aim;
+        public static string Throw;
+        public static string Pause;
+
+        public static string Submit;
+        public static string Cancel;
+        public static string Switch;
+        public static string Rebind;
+
+        public static string[] AllActions;
+
+        public static void Initialize(InputActionsSO inputActions)
+        {
+            KeyboardScheme = inputActions.KeyboardScheme;
+            GamepadScheme = inputActions.GamepadScheme;
+
+            HumanoidActionMap = inputActions.HumanoidActionMap;
+            SingularityActionMap = inputActions.SingularityActionMap;
+            UIActionMap = inputActions.UIActionMap;
+            InactiveActionMap = inputActions.InactiveActionMap;
+
+            Move = inputActions.Move;
+            Look = inputActions.Look;
+            Jump = inputActions.Jump;
+            Dash = inputActions.Dash;
+            Slide = inputActions.Slide;
+            Aim = inputActions.Aim;
+            Throw = inputActions.Throw;
+            Pause = inputActions.Pause;
+
+            Submit = inputActions.Submit;
+            Cancel = inputActions.Cancel;
+            Switch = inputActions.Switch;
+            Rebind = inputActions.Rebind;
+
+            AllActions = new string[]
+            {
+            Move, Look, Jump, Dash, Slide, Aim, Throw, Pause, Submit, Cancel
+            };
+        }
+    }
+
+}
