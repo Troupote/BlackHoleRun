a849d2d0265487f43dd11023dc4a121e452dc660
Furydrack <marius.boulandet@gmail.com>
2025-05-01 10:03:32 +0200

    [FEAT] Settings Manager + Resolution setting
    - Settings manager do the changes when a settings is changed
    - Placed it in InGameTest and MainMenu
    - Resolution setting : now can choose a resolution etc (not working in editor obviously, to test)

---
+++ Assets/_Game/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace BHR
+{
+    public class SettingsManager : ManagerSingleton<SettingsManager>
+    {
+        #region Global settings
+
+        public void UpdateResolutionAndWindowed()
+        {
+            // Resolution
+            string[] resolution = SettingsSave.LoadResolution().Split(SettingsSave.RESOLUTION_CAST);
+            bool useDefault = resolution.Length != 2;
+
+            int width = useDefault ? Screen.width : int.Parse(resolution[0]);
+            int height = useDefault ? Screen.height : int.Parse(resolution[1]);
+
+            // Windowed or not
+            FullScreenMode mode = SettingsSave.LoadIsWindowed() switch { -1 => Screen.fullScreenMode, 0 => FullScreenMode.ExclusiveFullScreen, 1 => FullScreenMode.Windowed, _ => Screen.fullScreenMode };
+
+            Screen.SetResolution(width, height, mode);
+        }
+        #endregion
+    }
+}
---
+++ Assets/_Game/UI/Scripts/SettingsLinker/ResolutionUILinker.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace BHR.UILinkers
+{
+    public class ResolutionUILinker : SettingsUILinker<int>
+    {
+        public override void SaveSetting(int choice)
+        {
+            SettingsSave.SaveResolution(GetComponent<TMP_Dropdown>().options[choice].text);
+            SettingsManager.Instance.UpdateResolutionAndWindowed();
+        }
+
+        protected override void LoadSetting()
+        {
+            TMP_Dropdown dropdown = GetComponent<TMP_Dropdown>();
+
+            TMP_Dropdown.OptionData currentResolution = dropdown.options.FirstOrDefault(o => o.text == SettingsSave.LoadResolution());
+            if(currentResolution == null)
+            {
+                // Default detected resolution
+                string resolutionText = $"{Screen.width} {SettingsSave.RESOLUTION_CAST} {Screen.height}";
+                currentResolution = dropdown.options.FirstOrDefault(o => o.text == resolutionText);
+
+                if(currentResolution == null) // if detected resolution isn't in the game resolutions
+                {
+                    // @todo réfléchir à quoi faire
+                    Debug.LogWarning($"{resolutionText} ne fait pas partie des résolutions proposées par BlackHoleRun");
+                }
+            }
+
+            dropdown.value = dropdown.options.IndexOf(currentResolution);
+        }
+    }
+}
