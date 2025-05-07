70822097e4068985bf425df32db8a23ce60f2a93
Troupote <thybault.jallu@icloud.com>
2025-05-07 11:22:43 +0200

    [Content] Sound Events Update and sound bank update

--- Assets/_Game/Scripts/Music/FmodEventsCreator.cs
+++ Assets/_Game/Scripts/Music/FmodEventsCreator.cs
@@ -10,6 +10,21 @@ public class FmodEventsCreator : MonoBehaviour
 	[field: Header("Throw Black Hole")]
     [field: SerializeField] public EventReference blackHoleTrowSfx {get; private set;}
     
+    [field: Header("Level Start")]
+    [field: SerializeField] public EventReference levelStart {get; private set;}
+    
+    [field: Header("Exit Singularity")]
+    [field: SerializeField] public EventReference exitSingularity {get; private set;}
+    
+    [field: Header("Standby Singularity")]
+    [field: SerializeField] public EventReference standbySingularity {get; private set;}
+    
+    [field: Header("Jump")]
+    [field: SerializeField] public EventReference jump {get; private set;}
+    
+    [field: Header("Dash")]
+    [field: SerializeField] public EventReference dash {get; private set;}
+    
     public static FmodEventsCreator instance;
     
     private void Awake()
