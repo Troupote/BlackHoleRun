ba179c841a9cc75ebb1b00773d111a5464c4637a
Furydrack <marius.boulandet@gmail.com>
2025-06-14 10:21:59 +0200

    [CHORE] Generate materials instead of use existing one

--- Assets/_Game/Scripts/LevelObjects/BeltController.cs
+++ Assets/_Game/Scripts/LevelObjects/BeltController.cs
@@ -7,7 +7,9 @@ namespace BHR
     public class BeltController : MonoBehaviour
     {
         [SerializeField] private Vector2 _speedFork;
-        [SerializeField] private Material[] _rotateMats;
+        [SerializeField] private Material _rotateMat;
+        [SerializeField] private int _randomMatGenerationCount;
+        private List<Material> _runtimeMats = new List<Material>();
         private bool _init = false;
         private float _angle = 0f;
 
@@ -18,9 +20,13 @@ namespace BHR
         }
         private void InitMats()
         {
+            _runtimeMats.Clear();
             // Set random mat
-            foreach (Material mat in _rotateMats)
+            for(int i=0; i<_randomMatGenerationCount;++i)
             {
+                Material mat = new Material(_rotateMat);
+                _runtimeMats.Add(mat);
+                
                 mat.SetFloat("_GameTimeScale", 1f);
                 mat.SetFloat("_Speed", Random.Range(_speedFork.x, _speedFork.y));
                 mat.SetVector("_Direction", new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
@@ -32,14 +38,14 @@ namespace BHR
             // Set random mat on each rock
             foreach (Transform rock in transform)
                 if (rock.name.Contains("rock", System.StringComparison.OrdinalIgnoreCase))
-                    rock.GetComponent<Renderer>().material = _rotateMats[Random.Range(0, _rotateMats.Length)];
+                    rock.GetComponent<Renderer>().material = _runtimeMats[Random.Range(0, _runtimeMats.Count)];
 
             _init = true;
         }
 
         private void SetMats(float angle)
         {
-            foreach (Material mat in _rotateMats)
+            foreach (Material mat in _runtimeMats)
                 mat.SetFloat("_Angle", angle);
         }
 
