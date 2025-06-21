2d08b5146c96261445c1dbcc792a03f5e0de54c9
Furydrack <marius.boulandet@gmail.com>
2025-06-21 08:25:40 +0200

    [FEAT] Star Wars Credits (wip)
    - Need to update text at end of project
    - Bug at the end of scroll -> text disappears from camera renders ?

---
+++ Assets/_Game/UI/Scripts/ModulesUI/CreditsUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BHR
+{
+    public class CreditsUI : AModuleUI
+    {
+        [SerializeField] private string _sceneName = "Credits";
+        private void OnEnable()
+        {
+            ScenesManager.Instance.LoadSceneAdditive(_sceneName);
+        }
+
+        public override void Back()
+        {
+            ScenesManager.Instance.UnloadSceneAdditive(_sceneName);
+            base.Back();
+        }
+    }
+}
